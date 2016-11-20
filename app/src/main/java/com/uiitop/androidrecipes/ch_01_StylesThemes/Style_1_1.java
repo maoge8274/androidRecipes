ec61e227f1bed4d8395194249e3f0338c07d4337
leohe <hemaojun@foxmail.com>
2016-11-20 21:37:32 +0800

    edit

---
+++ app/src/main/java/com/uiitop/androidrecipes/ch_01_StylesThemes/Style_1_1.java
@@ -0,0 +1,15 @@
+package com.uiitop.androidrecipes.ch_01_StylesThemes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.uiitop.androidrecipes.R;
+
+public class Style_1_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_style_1_1);
+    }
+}
---
+++ app/src/main/java/com/uiitop/androidrecipes/model/ComponentItem.java
@@ -0,0 +1,10 @@
+package com.uiitop.androidrecipes.model;
+
+/**
+ * Created by leohe on 2016/11/20.
+ */
+
+public class ComponentItem {
+    private String title;
+    private String name;
+}
